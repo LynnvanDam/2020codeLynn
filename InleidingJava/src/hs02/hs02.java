49653f6e0544a254ff80bedf87616c528d1d19b0
LynnvanDam <lvandam@roc-dev.com>
2020-09-11 10:12:24 +0200

    volgende opdracht gedaan: roepnaam en  achternaam onder elkaar afgedrukt. achtergrond is wit, roepnaam blauw en achternaam in rood.

--- InleidingJava/src/hs02/hs02.java
+++ InleidingJava/src/hs02/hs02.java
@@ -6,12 +6,14 @@ import java.awt.*;
 public class hs02 extends Applet {
 
     public void init() {
-        setBackground(Color.BLUE);
+        setBackground(Color.white);
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.YELLOW);
+        g.setColor(Color.BLUE);
         g.drawString("Lynn", 100, 100);
+        g.setColor(Color.RED);
+        g.drawString("van Dam",100,110);
     }
 
 }
