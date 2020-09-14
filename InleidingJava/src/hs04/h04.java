4e160991751ff47653f01a94c09f8e15ea40cb94
LynnvanDam <lvandam@roc-dev.com>
2020-09-14 09:59:02 +0200

    begonnen hoofdstuk 4, opdracht 4.1 gedaan (rechtbenige driehoek)

---
+++ InleidingJava/src/hs04/h04.java
@@ -0,0 +1,19 @@
+package hs04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class h04 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(100,50,50,100);
+        g.drawLine(100,50,150,100);
+        g.drawLine(50,100,150,100);
+    }
+
+
+
+}
