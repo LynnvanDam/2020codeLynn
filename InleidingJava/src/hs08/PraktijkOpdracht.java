4c17d9aa55a6b212b5d5adb0886cec6d4f302890
LynnvanDam <lvandam@roc-dev.com>
2020-10-01 13:52:09 +0200

    praktijkopdracht afgemaakt, buttons werking gegeven

--- InleidingJava/src/hs08/PraktijkOpdracht.java
+++ InleidingJava/src/hs08/PraktijkOpdracht.java
@@ -2,12 +2,14 @@ package hs08;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PraktijkOpdracht extends Applet {
 
     Button plus, min, keer, delen;
     TextField een, twee;
-    double getal, uitkomst;
+    double getal,getaltwee, uitkomst;
     int breedte, hoogte;
 
 
@@ -21,13 +23,17 @@ public class PraktijkOpdracht extends Applet {
         delen = new Button("/");
         een = new TextField();
         twee = new TextField();
-
         add(plus);
         add(min);
         add(keer);
         add(delen);
         add(een);
         add(twee);
+
+        plus.addActionListener(new PlusListener());
+        min.addActionListener(new MinListener());
+        keer.addActionListener(new KeerListener());
+        delen.addActionListener(new DeelListener());
     }
 
     public void paint(Graphics g) {
@@ -45,5 +51,57 @@ public class PraktijkOpdracht extends Applet {
         delen.setSize(breedte,hoogte);
         delen.setLocation(300,10);
 
+    }
+
+    class PlusListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String plussom = een.getText();
+            String plustwee = twee.getText();
+            getal = Double.parseDouble(plussom);
+            getaltwee = Double.parseDouble(plustwee);
+            uitkomst = getal + getaltwee;
+            een.setText("" + uitkomst);
+            twee.setText("");
+            repaint();
+        }
+    }
+
+    class MinListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String minsom = een.getText();
+            String mintwee = twee.getText();
+            getal = Double.parseDouble(minsom);
+            getaltwee = Double.parseDouble(mintwee);
+            uitkomst = getal - getaltwee;
+            een.setText("" + uitkomst);
+            twee.setText("");
+            repaint();
+        }
+    }
+
+    class KeerListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String keersom = een.getText();
+            String keertwee = twee.getText();
+            getal = Double.parseDouble(keersom);
+            getaltwee = Double.parseDouble(keertwee);
+            uitkomst = getal * getaltwee;
+            een.setText("" + uitkomst);
+            twee.setText("");
+            repaint();
+        }
+    }
+
+    class DeelListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String deelsom = een.getText();
+            String deeltwee = twee.getText();
+            getal = Double.parseDouble(deelsom);
+            getaltwee = Double.parseDouble(deeltwee);
+            uitkomst = getal / getaltwee;
+            een.setText("" + uitkomst);
+            twee.setText("");
+            repaint();
+        }
     }
 }
