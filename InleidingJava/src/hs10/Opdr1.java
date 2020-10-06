71b0d6c323068d43b7a9d94a0aaa9f5970a431c1
LynnvanDam <lvandam@roc-dev.com>
2020-10-06 13:34:11 +0200

    begonnen opdracht 1 hoofdstuk 10

---
+++ InleidingJava/src/hs10/Opdr1.java
@@ -0,0 +1,13 @@
+package hs10;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr1 extends Applet {
+
+    TextField tekstvak;
+    Label label;
+    String tekst, tekst2;
+    int getal;
+
+}
