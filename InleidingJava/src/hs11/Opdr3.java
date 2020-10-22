f6f07da3c7ad2fd9ee9e2b5e6e5133f4eed72b26
LynnvanDam <lvandam@roc-dev.com>
2020-10-22 11:34:35 +0200

    opdracht 11.3 gemaakt

---
+++ InleidingJava/src/hs11/Opdr3.java
@@ -0,0 +1,24 @@
+package hs11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr3 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        int teller, getal;
+        int y = 0, cijfer = 0;
+
+        for (teller = 1; teller >= 0;){
+            y += 20;
+            getal = cijfer + teller;
+            g.drawString("" + getal,10,y);
+            cijfer = teller;
+            teller = getal;
+        }
+    }
+}
