621ba50f981f3ea707d463f9da50d9c5b990af40
LynnvanDam <lvandam@roc-dev.com>
2020-10-22 13:08:44 +0200

    opdracht 11.7 gemaakt

---
+++ InleidingJava/src/hs11/Opdr7.java
@@ -0,0 +1,24 @@
+package hs11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr7 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        int width, length, x = 300, y = 300;
+        int cirkels;
+
+        for (cirkels = 0, width = 0, length = 0;cirkels <= 49;cirkels++) {
+            width += 10;
+            length += 10;
+            x -= 5;
+            y -= 5;
+            g.drawOval(x, y, width, length);
+        }
+    }
+}
