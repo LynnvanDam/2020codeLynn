1289ee959d1d2784c2dc3c11375b8ff0ff806bea
LynnvanDam <lvandam@roc-dev.com>
2020-10-27 14:17:20 +0100

    Hoofdstuk 11 praktijkopdracht pt 1 gemaakt

--- InleidingJava/src/hs11/Praktijkopdracht.java
+++ InleidingJava/src/hs11/Praktijkopdracht.java
@@ -8,18 +8,22 @@ import java.awt.event.ActionListener;
 public class Praktijkopdracht extends Applet {
 
     TextField textvak;
-    Button ok;
-    int getal, stuff, begin, som;
-    int y = 60;
+    Button ok, reset;
+    int y = 40;
+    int getal;
     String s;
+    String string = "";
 
     public void init() {
         textvak = new TextField();
         ok = new Button("ok");
+        reset = new Button("Reset");
         add(textvak);
         add(ok);
+        add(reset);
 
         ok.addActionListener(new OkListener());
+        reset.addActionListener(new ResetListener());
     }
 
     public void paint(Graphics g) {
@@ -27,20 +31,32 @@ public class Praktijkopdracht extends Applet {
         textvak.setLocation(10,10);
         ok.setSize(40,30);
         ok.setLocation(90,10);
+        reset.setSize(40,30);
+        reset.setLocation(140,10);
 
-        for (begin = 0; stuff < 9; begin++) {
-            som = begin * getal;
-            y += 20;
-            g.drawString("" + begin + " * " + getal + " = " + som, 10, y);
-        }
     }
 
     class OkListener implements ActionListener{
         public void actionPerformed(ActionEvent e) {
             s = textvak.getText();
             getal = Integer.parseInt(s);
-            stuff = getal;
+            int tafel, som;
+
+            for (tafel = 0; tafel < 11; tafel++){
+                som = getal * tafel;
+                y += 20;
+                string = "" + tafel + " * " + getal + " = " + som;
+                getGraphics().drawString(string,10,y);
+            }
+        }
+    }
+
+    class ResetListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            string = "";
+            y = 40;
             repaint();
         }
     }
 }
+
