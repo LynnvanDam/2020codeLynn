e4511e88148ced6dd94189f599abf955822781a2
LynnvanDam <lvandam@roc-dev.com>
2020-10-30 14:28:38 +0100

    Opdracht 12.3 begonnen moet nog nummers op volgorde in tekstvakken zetten

---
+++ InleidingJava/src/hs12/Opdr3.java
@@ -0,0 +1,42 @@
+package hs12;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+public class Opdr3 extends Applet {
+
+    TextField [] vak;
+    Button ok;
+
+    public void init() {
+        vak = new TextField[5];
+        int teller;
+        for (teller = 0; teller < vak.length; teller++){
+            vak[0] = new TextField();
+            add(vak[0]);
+        }
+
+        ok = new Button("ok");
+        add(ok);
+        ok.addActionListener(new OkListener());
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    class OkListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            int[] s = new int[5];
+            int i;
+            for (i = 0; i < vak.length; i++){
+                s[i] = Integer.parseInt(vak[i].getText());
+            }
+
+
+        }
+    }
+}
