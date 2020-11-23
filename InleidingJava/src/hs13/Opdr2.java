ddd8ff76d10813d8fc2c596fe0eccdf9376ed188
LynnvanDam <lvandam@roc-dev.com>
2020-11-23 12:59:34 +0100

    13.2 moet alleen nog lijnen niet parallel laten lopen

--- InleidingJava/src/hs13/Opdr2.java
+++ InleidingJava/src/hs13/Opdr2.java
@@ -15,7 +15,7 @@ public class Opdr2 extends Applet {
 
     void tekenBaksteen(Graphics g){
         g.setColor(Color.WHITE);
-        int i, b;
+        int i;
         int x1 = 0;
         int x2 = 270;
         int y = 5;
@@ -24,8 +24,11 @@ public class Opdr2 extends Applet {
             y += 15;
         }
 
-        int y1, y2, x3, x4;
-        for (y1 = 5, y2 = 20, x3 = 10, x4 = 10, i = 0; i < 10; i++){
+        int y1 = 5;
+        int y2 = 20;
+        int x3 = 10;
+        int x4 = 10;
+        for (i = 0; i < 10; i++){
             g.drawLine(x3,y1,x4,y2);
             x3 += 30;
             x4 += 30;
@@ -33,12 +36,11 @@ public class Opdr2 extends Applet {
                 i = 0;
                 x3 = 10;
                 x4 = 10;
-                y1 += 30;
-                y2 += 30;
+                y1 += 15;
+                y2 += 15;
             }
         }
 
-
-
     }
+
 }
