f06fed6c034f75755003e5afcbaa0a26ac967ef6
LynnvanDam <lvandam@roc-dev.com>
2020-12-10 13:39:03 +0100

    13.3 af

--- InleidingJava/src/hs13/Opdr3.java
+++ InleidingJava/src/hs13/Opdr3.java
@@ -8,36 +8,41 @@ import java.awt.event.ActionListener;
 public class Opdr3 extends Applet {
 
     Button baksteen, beton;
+    int width = 0;
+    int heigth = 0;
+    Color color;
+    int widthScreen; int heigthScreen;
 
     public void init() {
-        //baksteen = new Button("baksteen");
-       // beton = new Button("beton");
-       // add(baksteen);
-       // add(beton);
 
+        widthScreen = 1000; heigthScreen = 1000;
+        setSize(widthScreen, heigthScreen);
+        baksteen = new Button("baksteen");
+        beton = new Button("beton");
+        add(baksteen);
+        add(beton);
+
+        baksteen.addActionListener(new BaksteenClass());
+        beton.addActionListener(new BetonClass());
         setBackground(Color.red);
-        setSize(500, 500);
     }
 
     public void paint(Graphics g) {
-        tekenMuur(g, 20,10, Color.white );
+        if (width != 0 && heigth != 0) tekenMuur(g, width,heigth, color );
     }
 
     void tekenMuur (Graphics g, int width, int height, Color color) {
         g.setColor(color);
-        int numberOfBricksRow = 500 / width;
-        int numberOfBricksColumn = 500 / height;
+        int numberOfBricksRow = widthScreen / width;
+        int numberOfBricksColumn = heigthScreen / height;
         int x = 0;
         int y = 0;
         for (int rows = 0; rows < numberOfBricksColumn; rows++){
             if (rows % 2 == 0){
-                for (int column = 0; column < numberOfBricksRow; column++){
-                    g.drawRect(x,y,width,height);
-                    x += width;
-                }
+                x = this.drawRowOfBricks(numberOfBricksRow, g, x, y, width, height);
             }  else {
                 g.drawRect(0, y, width / 2, height);
-                g.drawRect(500 - width / 2, y, width /2 , height);
+                g.drawRect(widthScreen - width / 2, y, width /2 , height);
                 x = width / 2;
                 for (int column = 0; column < numberOfBricksRow - 1; column++){
                     g.drawRect(x,y,width,height);
@@ -50,11 +55,27 @@ public class Opdr3 extends Applet {
         }
 
     }
-
-    void tekenBeton (Graphics g){
-
+    private int drawRowOfBricks (int numberOfBricksRow, Graphics g, int x , int y, int width, int height){
+        for (int column = 0; column < numberOfBricksRow; column++){
+            g.drawRect(x,y,width,height);
+            x += width;
+        }
+        return x;
+    }
+    class BaksteenClass implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            width = 20; heigth = 10; color = Color.WHITE;
+            setBackground(Color.red);
+            repaint();
+        }
     }
 
-
+    class BetonClass implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            width = 50; heigth = 20; color = Color.WHITE;
+            setBackground(Color.gray);
+            repaint();
+        }
+    }
 
 }
